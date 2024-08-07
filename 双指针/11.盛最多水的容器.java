ac6c9019b671836867b3c9c37d9ae0dc3a833196
Claviay <137407542+Claviay@users.noreply.github.com>
2024-08-08 01:51:02 +0800

    Update 11.盛最多水的容器.java

--- 双指针/11.盛最多水的容器.java
+++ 双指针/11.盛最多水的容器.java
@@ -2,10 +2,12 @@
 class Solution {
     public int maxArea(int[] height) {
         int i = 0, j = height.length - 1, res = 0;
-        
-        
+
+
         // 这个逻辑成立的原因是，比如，看这个两个：如果左边的一个板子a，以及右边板子b
         // 如果a比b高，那么宽和高都强的情况下，显然a不应该右移
+        
+        // 只有较小的那个先移动，才有可能得到更大的结果
         while(i < j) {
             res = height[i] < height[j] ?
                     Math.max(res, (j - i) * height[i++]):
