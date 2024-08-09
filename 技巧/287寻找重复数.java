80a267a4df7c2d6db0d415e2341f6d787d712e83
Claviay <137407542+Claviay@users.noreply.github.com>
2024-08-09 21:11:33 +0800

    Update 287寻找重复数.java
    快慢指针

--- 技巧/287寻找重复数.java
+++ 技巧/287寻找重复数.java
@@ -1,35 +1,33 @@
 /*
-遍历数组：
-对于每个元素 nums[i]，如果它没有在正确的位置上（即 nums[i] != i + 1），
-则尝试将它放到正确的位置上。
-
-检测重复数字：
-如果发现 nums[nums[i] - 1] 已经等于 nums[i]，这
-说明当前数字已经存在于数组中，因此这个数字就是重复的。
-
-交换元素：
-如果 nums[i] 没有在正确的位置上，并且该位置的数字与 nums[i] 不同，
-则交换这两个数字。通过不断交换，最终每个数字应该都能放置到它正确的位置上，除非这个数字是重复的。
+快慢指针
  */
 class Solution {
-    public static int findDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] != i + 1) { // 如果这个数据不在正确位置
-                if (nums[nums[i] - 1] == nums[i]) {  // 看看这个数据对应的正确位置是否已经有了
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
 
-                    // 如果已经有了，那么说明找到重复的数字了
-                    return nums[i];  
-                }
-                // 交换 nums[i] 和 nums[nums[i] - 1]
-                int temp = nums[nums[i] - 1];
-                nums[nums[i] - 1] = nums[i];
-                nums[i] = temp;
-            }
+        // 初始化 slow 和 fast 指针，slow 一次走一步，fast 一次走两步
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+
+        // 快慢指针循环，直到它们相遇
+        while(slow != fast){
+            slow = nums[slow]; // slow 每次走一步
+            fast = nums[nums[fast]]; // fast 每次走两步
         }
-        return -1;  // 没有找到重复的数字，通常情况下不应发生
+
+        // 快慢指针相遇后，重置一个指针到起点，两个指针每次都走一步
+        int pre1 = 0;
+        int pre2 = slow;
+
+        while(pre1 != pre2){
+            pre1 = nums[pre1]; // pre1 每次走一步
+            pre2 = nums[pre2]; // pre2 每次走一步
+        }
+
+        // 当两个指针再次相遇时，它们在环的起点，即重复的数字
+        return pre1;
     }
 
-    public static void main(String[] args) {
-        System.out.println(findDuplicate(new int[]{1, 2, 3, 4, 5, 6, 2, 7, 8, 9}));
-    }
+    
 }
