a2c88b5d30ae6a9162a92b572538730ccbe2a537
Claviay <137407542+Claviay@users.noreply.github.com>
2024-08-16 16:16:09 +0800

    Create 70爬楼梯.java

---
+++ 动态规划/70爬楼梯.java
@@ -0,0 +1,22 @@
+class Solution {
+    // 一次爬一个或者两个台阶
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().climbStairs(2)); // 2
+        System.out.println(new Solution().climbStairs(3)); // 3
+
+    }
+}
