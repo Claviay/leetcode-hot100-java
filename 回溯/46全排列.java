73a528a8b40c8abd7ad5b2e248d971d591eba69a
Claviay <137407542+Claviay@users.noreply.github.com>
2024-08-29 02:05:14 +0800

    Create 46全排列.java

---
+++ 回溯/46全排列.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        boolean[] onPath = new boolean[nums.length];
+        
+        dfs(nums, ans, path, onPath);
+        return ans;
+    }
+
+    public void dfs(int[] nums, List<List<Integer>> ans, List<Integer> path, boolean[] onPath) {
+        if (path.size() == nums.length) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!onPath[i]) {
+                // 添加当前元素到路径
+                path.add(nums[i]);
+                // 标记当前元素为已使用
+                onPath[i] = true;
+
+                // 递归调用
+                dfs(nums, ans, path, onPath);
+                
+                // 回溯
+                path.remove(path.size() - 1);
+                onPath[i] = false;
+            }
+        }
+    }
+}
