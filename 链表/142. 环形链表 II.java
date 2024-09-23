b12272bed6777bb177662067b0ce585a1c312e9e
Claviay <137407542+Claviay@users.noreply.github.com>
2024-09-23 14:14:19 +0800

    Create 142. 环形链表 II.java

---
+++ 链表/142. 环形链表 II.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next == null) {
+                return null;
+            } else {
+                fast = fast.next.next;
+            }
+
+            // 额外一点点变化
+            if (fast == slow) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+}
