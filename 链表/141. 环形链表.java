d6f019b4affedfe11fbaf6d064e15932851c1d99
Claviay <137407542+Claviay@users.noreply.github.com>
2024-09-23 13:44:11 +0800

    Update 141. 环形链表.java

--- 链表/141. 环形链表.java
+++ 链表/141. 环形链表.java
@@ -1,32 +1,21 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        //////////////////////////////////////
         if (head == null || head.next == null) {
-            ///////////////////////////////////
-            return false;
+            return false; // 如果链表为空或只有一个节点，则不可能有环
         }
+
         ListNode slow = head;
         ListNode fast = head.next;
+
         while (slow != fast) {
-            ///////////////////////
             if (fast == null || fast.next == null) {
-            ////////////////////////
-                return false;
+                return false; // 如果快指针到达链表末尾，则没有环
             }
-            slow = slow.next;
-            fast = fast.next.next;
+
+            slow = slow.next; // 慢指针移动一步
+            fast = fast.next.next; // 快指针移动两步
         }
-        return true;
+
+        return true; // 如果慢指针和快指针相遇，则有环
     }
 }
