4c86d68f8d6887c930667e8d1c7a2bf4eff20a68
Claviay <137407542+Claviay@users.noreply.github.com>
2024-09-30 11:48:17 +0800

    Update 94. 二叉树的中序遍历.java

--- 二叉树/94. 二叉树的中序遍历.java
+++ 二叉树/94. 二叉树的中序遍历.java
@@ -36,8 +36,7 @@ class Solution {
 
 // 迭代
 
-/*
-
+中序
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<Integer>();  // 存储遍历结果的列表
@@ -60,4 +59,56 @@ class Solution {
 
 
 
-*/
+
+
+后序
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<Integer>();
+        LinkedList<TreeNode> stk = new LinkedList<TreeNode>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                res.addFirst(root.val);
+                stk.push(root);
+                root = root.right;
+            }
+            if (!stk.isEmpty()) {
+            root = stk.pop();
+            root = root.left;
+            }
+        }
+        return res;
+    }
+}
+
+
+
+
+前序
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val); // 访问当前节点
+            
+            // 先压入右子节点，再压入左子节点
+            // 因为栈是 LIFO（后进先出），所以左子节点会先处理
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        
+        return res;
+    }
+}
